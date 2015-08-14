0f1cb1b749e800584f7bb9ee07efeb280650d28b
Ethan Durrant <ethan.durrant@mulesoft.com>
2015-08-13 18:47:13 -0700

    working on basic functions

---
+++ routes/display.js
@@ -0,0 +1,81 @@
+//display routes
+
+var SerialPort = require("serialport").SerialPort;
+var serialPort;
+
+var display_data = {
+    displayString: " ",
+    textColor: 'orange',
+    textFont: 'default'   
+};
+
+var fonts = {
+    'default':      "\s",
+    '7by6':         "\s",
+    'short':        "\q",
+    'shortAndWide': "\\r",
+    'wide':         "\\t",
+    '7by9':         "\u",
+    'extraWide':    "\v",
+    'small':        "\w"
+};
+
+
+var effects = {
+    'immediate':"\rB\c"
+};
+
+var colors = {
+    'red':          "\a",
+    'brightRed':    "\b",
+    'orange':       "\c",
+    'brightOrange': "\d",
+    'yellow':       "\e",
+    'brightYellow': "\f",
+    'green':        "\g",
+    'brightGreen':  "\h",
+    'layerMix':     "\i",
+    'brightLayerMix':"\j",
+    'verticalMix':  "\k",
+    'sawtoothMix':  "\l",
+    'greenOnRed':   "\m",
+    'redOnGreen':   "\\n",
+    'orangeOnRed':  "\o",
+    'yellowOnGreen':"\p"
+};
+
+function writeDisplay(data, cb) {
+    var outStr = "";
+    outStr += "~128"; //code number 128
+    outStr += "~f01"; //file 01
+    outStr += effects['immediate']; //immediate effect
+    outStr += fonts[data.textFont]; //font
+    outStr += colors[data.textColor]; //color
+    outStr += data.displayString; //the string to display
+    outStr += '\r\r\r'; //ending
+    serialPort.write(outStr, function(err){
+        return cb(err); 
+    });
+}
+
+exports.initDisplay = function(options, cb){
+    var error = false;
+    serialPort = new SerialPort(options.port, {
+        baudrate: options.baud
+    }, function(err){
+        if(err) error = err;
+        return cb(error); 
+    });
+    //cb(error);
+};
+
+exports.getDisplay = function(req, res, next){
+    res.json(display_data);
+    return next();
+};
+
+exports.putDisplay = function(req, res, next){
+
+    
+};
+
---
+++ server.js
@@ -0,0 +1,54 @@
+#/usr/bin/env node
+
+var restify = require('restify');
+
+
+var server = restify.createServer({
+    name: 'display-api',
+    version: '1.0.0'
+});
+
+//setup parsers
+server.use(restify.bodyParser());
+server.use(restify.acceptParser(server.acceptable));
+
+//special handling for request from curl client
+server.pre(restify.pre.userAgentConnection());
+
+//use PORT variable passed from the shell or 7575
+var port = process.env.PORT || 7575;
+
+//get base path from shell or /display-api
+var base_path = process.env.DISPLAY_API_PATH || '/display-api';
+
+//display serial port
+var display_serial_port = process.env.DISPLAY_SERIAL || '/dev/ttyUSB0';
+//display baud rate
+var display_serial_baud = process.env.DISPLAY_BAUD || 9600;
+
+var display_options = {
+    port: display_serial_port,
+    baud: display_serial_baud 
+};
+
+var display = require('./routes/display');
+
+//welcome/test route
+server.get(base_path + '/', function(req,res,next){
+    res.json({ message: "welcome to the Display API!" });
+    return next();
+});
+
+server.get(base_path + '/display', display.getDisplay );
+server.put(base_path + '/display', display.putDisplay );
+
+//setup server
+server.listen(port, function(){
+    console.log('listening on port ' + port);
+    //init the display
+    display.initDisplay(display_options, function(err){
+        if(err) throw err;
+        console.log('done initializing');
+    });
+});
+
