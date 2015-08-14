b6d587a521a7bfc925a384ee062ad6f3b4248d12
Ethan Durrant <ethan.durrant@mulesoft.com>
2015-08-13 19:47:00 -0700

    got the api working

--- routes/display.js
+++ routes/display.js
@@ -10,50 +10,59 @@ var display_data = {
 };
 
 var fonts = {
-    'default':      "\s",
-    '7by6':         "\s",
-    'short':        "\q",
-    'shortAndWide': "\\r",
-    'wide':         "\\t",
-    '7by9':         "\u",
-    'extraWide':    "\v",
-    'small':        "\w"
+    'default':      '\\s',
+    '7by6':         '\\s',
+    'short':        '\\q',
+    'shortAndWide': '\\r',
+    'wide':         '\\t',
+    '7by9':         '\\u',
+    'extraWide':    '\\v',
+    'small':        '\\w'
 };
 
 
 var effects = {
-    'immediate':"\rB\c"
+    'immediate':'\rB\\c'
 };
 
 var colors = {
-    'red':          "\a",
-    'brightRed':    "\b",
-    'orange':       "\c",
-    'brightOrange': "\d",
-    'yellow':       "\e",
-    'brightYellow': "\f",
-    'green':        "\g",
-    'brightGreen':  "\h",
-    'layerMix':     "\i",
-    'brightLayerMix':"\j",
-    'verticalMix':  "\k",
-    'sawtoothMix':  "\l",
-    'greenOnRed':   "\m",
-    'redOnGreen':   "\\n",
-    'orangeOnRed':  "\o",
-    'yellowOnGreen':"\p"
+    'red':          '\\a',
+    'brightRed':    '\\b',
+    'orange':       '\\c',
+    'brightOrange': '\\d',
+    'yellow':       '\\e',
+    'brightYellow': '\\f',
+    'green':        '\\g',
+    'brightGreen':  '\\h',
+    'layerMix':     '\\i',
+    'brightLayerMix':'\\j',
+    'verticalMix':  '\\k',
+    'sawtoothMix':  '\\l',
+    'greenOnRed':   '\\m',
+    'redOnGreen':   '\\n',
+    'orangeOnRed':  '\\o',
+    'yellowOnGreen':'\\p'
 };
 
 function writeDisplay(data, cb) {
-    var outStr = "";
-    outStr += "~128"; //code number 128
-    outStr += "~f01"; //file 01
-    outStr += effects['immediate']; //immediate effect
-    outStr += fonts[data.textFont]; //font
-    outStr += colors[data.textColor]; //color
-    outStr += data.displayString; //the string to display
-    outStr += '\r\r\r'; //ending
-    serialPort.write(outStr, function(err){
+    //var outStr = "";
+    var outStr = ""
+            + '~128' //code number 128
+            + '~f01' //file 01
+            + "A" //auto effect
+            + effects['immediate'] //immediate effect
+            + colors[data.textColor] //color
+            + fonts[data.textFont] //font
+            + data.displayString //the string to display
+            + "\r\r\r"; //ending
+    var outBuff = new Buffer(outStr);
+    
+    serialPort.on('data', function(data){
+        console.log('data recieved: ' + data);
+    });
+    console.log('sending data: \n' + outBuff);
+    serialPort.write(outStr, function(err, results){
+        console.log('results: ' + results);
         return cb(err); 
     });
 }
@@ -75,7 +84,7 @@ exports.getDisplay = function(req, res, next){
 };
 
 exports.putDisplay = function(req, res, next){
-    if(req.params.displayString !== undefined){
+    if(req.params.displayString){
         display_data.displayString = req.params.displayString;
     }
     if(req.params.textColor !== undefined){
